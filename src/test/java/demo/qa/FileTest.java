1aeb1fa304dc7b234497d9a89e9959a554499bda
Леха <aorlov@sberhealth.ru>
2022-11-19 20:04:13 +0300

    add file tests

--- src/test/java/demo/qa/FileTest.java
+++ src/test/java/demo/qa/FileTest.java
@@ -1,10 +1,20 @@
 package demo.qa;
 
+import com.codeborne.pdftest.PDF;
+import com.codeborne.xlstest.XLS;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.opencsv.CSVReader;
 import model.Student;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import java.util.zip.ZipEntry;
+import java.util.zip.ZipFile;
+import java.util.zip.ZipInputStream;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -23,4 +33,45 @@ public class FileTest {
         assertThat(student.homework[1]).isEqualTo("Lesson2");
         assertThat(student.teacher).isEqualTo("Dmitrii Tuchs");
     }
+
+
+    ClassLoader cl = FileTest.class.getClassLoader();
+    @Test
+    void ReadingArchiveTest () throws Exception {
+        ZipFile zf = new ZipFile(new File("src/test/resources/Desktop.zip"));
+        try (ZipInputStream is = new ZipInputStream(cl.getResourceAsStream("Desktop.zip"))) {
+            ZipEntry entry;
+            while ((entry = is.getNextEntry()) != null) {
+                switch (entry.getName()) {
+                    case "import_empl.csv":
+                        try (InputStream inputStream = zf.getInputStream(entry);
+                             CSVReader reader = new CSVReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))) {
+                            List<String[]> content = reader.readAll();
+                            String[] row = content.get(0);
+                            String searchWords = row[0];
+                            assertThat(searchWords).isEqualTo("Name;ID;email");
+                        }
+                        break;
+                    case "import_ou.xlsx":
+                        try (InputStream inputStream = zf.getInputStream(entry)) {
+                            XLS xls = new XLS(inputStream);
+                            assertThat(
+                                    xls.excel.getSheetAt(0)
+                                            .getRow(0)
+                                            .getCell(0)
+                                            .getStringCellValue()
+                            ).isEqualTo("Name");
+                        }
+                        break;
+                    case "test.pdf":
+                        try (InputStream inputStream = zf.getInputStream(entry)) {
+                            PDF pdf = new PDF(inputStream);
+                            assertThat(pdf.author).isEqualTo("Yukon Department of Education");
+                        }
+                        break;
+                }
+            }
+        }
+    }
 }
+
